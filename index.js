2cc8a640d11fd72c3455a12822e8157a271ad48c
malicha <maalicha.maalicha@gmail.com>
2019-05-04 10:10:56 +0700

    belajar array

---
+++ index.js
@@ -0,0 +1,15 @@
+var siswa = [
+    ["malicha", 12, 'tkj',['bringin', 'semarang']],
+    ['laela', 12, 'rpl',['ngaliyan', 'semarang']],
+    ['kinanti', 12, 'tkj',['mangkang', 'semarang']]
+]
+
+const data = siswa[0][0] + siswa[0][3][1]
+console.log(data)
+
+
+var warna = ['merah', 'kuning','ungu']
+
+const dataWarna = warna[0] + warna [1]
+
+console.log(dataWarna)
